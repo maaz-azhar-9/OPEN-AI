2fd4304f51f26b2b14efe6a037a117c640a45bf7
Mohd Maaz Azhar <maazazhar9@gmail.com>
2025-08-19 18:09:52 +0530

    feat: clearing older messages

--- chat-app/src/basic-chat/index.js
+++ chat-app/src/basic-chat/index.js
@@ -1,8 +1,14 @@
 import OpenAI from 'openai';
+import { encoding_for_model } from 'tiktoken' 
+
 
 const openai = new OpenAI({
     apiKey: process.env.OPEN_AI_API_KEY
   });
+
+const MAX_TOKEN = 700;
+
+const encoder = encoding_for_model('gpt-3.5-turbo')
 
 const context = [{
     role: "system",
@@ -11,11 +17,15 @@ const context = [{
 
 async function  createChatCompletetion(){
     const response = await openai.chat.completions.create({
-        model:"chatgpt-4o-latest",
+        model:"gpt-3.5-turbo",
         messages: context
     })
 
     const responseMessage = response.choices[0].message;
+
+    if(response?.usage.total_tokens > MAX_TOKEN){
+        deleteOlderMessages();
+    }
 
     context.push({
         role:"assistant",
@@ -35,3 +45,33 @@ process.stdin.addListener('data', async function (input){
 
     await createChatCompletetion();
 })
+
+function deleteOlderMessages(){
+    let contextLength = getContextLength();
+
+    while(contextLength > MAX_TOKEN){
+        for(let i=0; i<context.length;i++){
+            if(context[i].role!=="system"){
+                context.splice(i,1);
+                contextLength = getContextLength();
+                console.log("new context length : ", contextLength);
+                break;
+            }
+        }
+    }
+}
+
+function getContextLength(){
+    let length = 0;
+    for(let i=0 ; i<context.length;i++){
+        if(typeof context[i].content === "string"){
+            length += encoder.encode(context[i].content).length;
+        }
+        else if(Array.isArray(context[i].content)){
+            context[i].content.forEach((messageContent) => {
+                length += encoder.encode(messageContent.content).length;
+            })
+        }
+    }
+    return length;
+}
